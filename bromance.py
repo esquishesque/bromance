05f0593e3ac3859c72bf395841812d2cb0a2c311
esquishesque <esquish@gmail.com>
2014-03-13 21:30:25 -0400

    robot movement implemented
    - made the robot's appearance reflect its orientation, and added
    rainbows
    - made getters and setters for each of location, orientation, and
    spawnloc
    - changed coordinate definition system from lists to named tuples
    - decided that location will always be 'Loc', and orientation always
    'Orient'
    - wrote functions updateLoc and updateOrient
    - wrote functions calling these, robotMove and robotTurn

--- bromance.py
+++ bromance.py
@@ -1,14 +1,7 @@
 from game import *
 
+
+
 if __name__ == '__main__':
     game = Game()
     print(game.board)
-
-
-
-#ilist = []
-#for i in range(5):
-    #ilist.append([])
-    #for j in range(5):
-        #ilist[i].append(j)
-        
--- game.py
+++ game.py
@@ -1,3 +1,8 @@
+from collections import namedtuple
+
+Location = namedtuple('Location', ['x','y'])
+
+
 class Game:
     """Creates Robot(s), creates a Board"""
     def __init__(self):
@@ -11,19 +16,42 @@ class Robot:
     """Names the Robot"""
     def __init__(self):
         self.playerName = "Player One"
-        self.appearance = "R"
-        self.spawnLoc = [0,0] # TODO have this assigned somehow during initialization (randomize, or something)
-        self.location = self.spawnLoc # This SHOULD be okay for initialization, but think about it
-        self.orientation = 2 # 0 is North, 1 is East, 2 is South, 3 is West
+        #self.appearance = "R"
+        self.spawnLoc = Location(0,0) #TODO have this assigned somehow during initialization (randomize, or something)
+        self.loc = self.spawnLoc # This SHOULD be okay for initialization, but think about it
+        self.orient = 2 # MUST be a value from 0-3; 0 is North, 1 is East, 2 is South, 3 is West
 
-    def getLocation(self):
-        return self.location
+    def getLoc(self):
+        return self.loc
+
+    def setLoc(self,x,y):
+        self.loc = Location(x,y)
+
+    def getOrient(self):
+        return self.orient
+
+    # %4 is mod4, so that the robot's orientation is always between 0-3
+    def setOrient(self,i):
+        self.orient = i%4
 
     def getSpawnLoc(self):
         return self.spawnLoc
 
+    def setSpawnLoc(self,x,y):
+        self.spawnLoc = Location(x,y)
+
     def __str__(self):
-        return self.appearance
+        #this makes the robot's appearance reflect its orientation
+        if self.getOrient() == 0:
+            return "^"
+        elif self.getOrient() == 1:
+            return ">"
+        elif self.getOrient() == 2:
+            return "v"
+        elif self.getOrient() == 3:
+            return "<"
+        else:                           #TODO should throw exception
+            print ("Unexpected value in robot.__str__")
         
 class Board:
     def __init__(self, robotList):
@@ -37,7 +65,7 @@ class Board:
         self.robotList = robotList
         
         #populate list of flag locations; crucially this is [x,y] NOT [row,col]
-        tempFlagPoint = [9,9] #TODO generate these better with a function later
+        tempFlagPoint = Location(9,9) #TODO generate these better with a function later
         self.flagLocList.append(tempFlagPoint)        
         
         # fill a grid representing a 10x10 board with Squares
@@ -47,11 +75,34 @@ class Board:
             self.grid.append([])
             for x in range(numCols):
                 self.grid[y].append([Square()])
-                if [x,y] in self.flagLocList:
+                if (x,y) in self.flagLocList:
                     self.grid[y][x].append(Flag())
                 for robot in self.robotList:
-                    if [x,y] == robot.getSpawnLoc():
+                    if (x,y) == robot.getSpawnLoc():
                         self.grid[y][x].append(Spawn())
+
+    def updateRoLoc(self,robot,x,y):
+        robot.setLoc(x,y)
+        print(self)
+
+    def updateRoOrient(self,robot,i):
+        robot.setOrient(i)
+        print(self)
+
+    def robotTurn(self,robot,numSpaces):
+        self.updateRoOrient(robot,robot.getOrient() + numSpaces)
+
+    def robotMove(self,robot,numSpaces):
+        if robot.getOrient() == 0: # facing north, subtract from y
+            self.updateRoLoc(robot,robot.getLoc().x,robot.getLoc().y-numSpaces)
+        elif robot.getOrient() == 1: # facing east, add to x
+            self.updateRoLoc(robot,robot.getLoc().x+numSpaces,robot.getLoc().y)
+        elif robot.getOrient() == 2: # facing south, add to y
+            self.updateRoLoc(robot,robot.getLoc().x,robot.getLoc().y+numSpaces)
+        elif robot.getOrient() == 3: # facing west, subtract x
+            self.updateRoLoc(robot,robot.getLoc().x-numSpaces,robot.getLoc().y)
+        else:                               #TODO should throw exception
+            print("Unexpected value in robotMove")
 
     def __str__(self):
         """Prints a properly formatted grid"""
@@ -61,7 +112,7 @@ class Board:
                 for square in range(len(self.grid[row][col])):
                     output = output + str(self.grid[row][col][square])
                 for robot in self.robotList:
-                    if [col,row] == robot.getLocation():
+                    if (col,row) == robot.getLoc():
                         output = output + str(robot)
                 output = output + '|'
             output = output + "\n"
